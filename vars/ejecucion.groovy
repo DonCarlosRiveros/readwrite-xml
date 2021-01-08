a072744842bda54c2b29d220eeab0abe8f3a301c
Carlos M. Riveros C <carlos.riveros@gmail.com>
2021-01-08 13:44:59 -0300

    Actualizacion con contenido local

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -14,32 +14,15 @@ def call()
 
     /* String[] arr_version */
 
-    figlet 'Antes'
-
     var_version = new File(archivo).getText('UTF-8')
-    println var_version
     var_largo = var_version.length();
-    println "Largo string : " + var_largo;
-
-    figlet 'Paso 1'
-
     var_inversa = var_version.reverse();
-    println var_inversa
-
     var_pos = var_inversa.indexOf('.');
-    println 'Posicion del . en texto : ' + var_pos;
     var_nropos = var_pos.toInteger();
-    println 'Posicion del . en numero : ' + var_nropos;
-
     var_release = var_inversa.substring(1, var_nropos);
     var_newrelease = var_release.toInteger() + 1;
-
-    println "Release : " + var_release;
-
     figlet 'Paso 2'
-
     var_nropos = var_nropos + 1;
-
     var_newversion = var_version.substring(1, (var_largo-var_nropos)) + '.' + var_newrelease;
 
     print("Version archivo:")
@@ -49,5 +32,5 @@ def call()
 
     println var_newversion
 
-
+    archivo.write(var_newversion);
 }
