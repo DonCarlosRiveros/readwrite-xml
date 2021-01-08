3c743d88427abed6e1419ee9af61331dbbc024e3
Carlos M. Riveros C <carlos.riveros@gmail.com>
2021-01-08 20:36:16 -0300

    Actualizacion con contenido local

--- vars/release_upgrade.groovy
+++ vars/release_upgrade.groovy
@@ -25,7 +25,7 @@ def call()
     File file_update = new File(archivo)
     file_update.write(var_newversion);
 
-    nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'txt', filePath: 'versiones.txt']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'txt', version: '1.0.0']]]
+    nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'txt', filePath: 'DevOpsUsach2020-1.0-versiones.txt']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'txt', version: '1.0.0']]]
 
     return archivo;
 }
