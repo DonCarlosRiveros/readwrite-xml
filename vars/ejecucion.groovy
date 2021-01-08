2760754dfff256cb6c259963a12363dd7d543129
Carlos M. Riveros C <carlos.riveros@gmail.com>
2021-01-08 11:11:41 -0300

    Actualizacion con contenido local

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -13,8 +13,8 @@ def call()
     String var_newversion = ''
     String[] arr_version
 
-    var_version = xml.release[0].version
     arr_version = var_version.split('.');
+    var_version = xml.release[0].version
     var_paso    = ((arr_version[2].toInteger)+1).toString;
 
     var_newversion = arr_version[0] + '.' + arr_version[1] + var_paso
