e32ed1cf9d3c0d94e46193120ac304b53e5714f6
Carlos M. Riveros C <carlos.riveros@gmail.com>
2021-01-08 20:31:43 -0300

    Actualizacion con contenido local

--- vars/release_upgrade.groovy
+++ vars/release_upgrade.groovy
@@ -1,6 +1,7 @@
 def call()
 {
-	def archivo = "/SSD/Personales/Dip-DevOps/versiones.txt"
+    sh 'curl -X GET -u admin:criveros http://localhost:8082/repository/test-nexus/com/devopsusach2020/DevOpsUsach2020/1.0/DevOpsUsach2020-1.0-versiones.txt -O'
+    def archivo = "DevOpsUsach2020-1.0-versiones.txt"
 
 	String var_version    = ''
     String var_inversa    = ''
@@ -23,4 +24,10 @@ def call()
 
     File file_update = new File(archivo)
     file_update.write(var_newversion);
+
+    nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'txt', filePath: 'versiones.txt']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'txt', version: '1.0.0']]]
+
+    return archivo;
 }
+
+return this
