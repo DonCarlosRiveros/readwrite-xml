7b21e6b45af69be92eaf96964ce7f1cb07b9cd65
Carlos M. Riveros C <carlos.riveros@gmail.com>
2021-01-08 11:00:31 -0300

    Actualizacion con contenido local

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -3,13 +3,27 @@ import groovy.util.*
 
 def call()
 {
-	def parser  = new XmlParser()
-	def archivo = parser.parse("/SSD/Personales/Dip-DevOps/readwrite-xml/resources/versiones.xml")
+	def archivoxml = "/SSD/Personales/Dip-DevOps/readwrite-xml/resources/versiones.xml"
+    def parser     = new XmlParser()
+	def archivo    = parser.parse()
+    def xml        = new XmlParser().parse(archivoxml)
 
-	String var_version = ''
-		
-    var_version = archivo.release.version
+	String var_version    = ''
+    String var_paso       = ''
+    String var_newversion = ''
+    String[] arr_version
+
+    var_version = xml.release[0].version
+    arr_version = var_version.split('.');
+    var_paso    = ((arr_version[2].toInteger)+1).toString;
+
+    var_newversion = arr_version[0] + '.' + arr_version[1] + var_paso
+
     print("Version archivo:")
     println var_version
+
+    print("Nueva Version archivo:")
+    println var_newversion
+
 
 }
