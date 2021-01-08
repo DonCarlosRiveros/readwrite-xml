616ef215eef0eeb82c26bc61a5c234f548bf719c
Carlos M. Riveros C <carlos.riveros@gmail.com>
2021-01-08 14:22:27 -0300

    Actualizacion con contenido local

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -15,26 +15,12 @@ def call()
     var_version = new File(archivo).getText('UTF-8');
     var_largo = var_version.length();
     var_inversa = var_version.reverse();
-    println 'Inversa : ' + var_inversa;
     var_pos = var_inversa.indexOf('.');
-    println 'var_pos : ' + var_pos;
     var_nropos = var_pos.toInteger();
-    println 'var_nropos : ' + var_nropos;
     var_release = var_inversa.substring(0, var_nropos);
-    println 'var_release : ' + var_release;
-    figlet 'antes'
     var_newrelease = var_release.toInteger() + 1;
-    figlet 'despues'
-    var_nropos = var_nropos + 1;
-    
+    var_nropos = var_nropos + 1;    
     var_newversion = var_version.substring(0, (var_largo-var_nropos)) + '.' + var_newrelease;
-
-    print("Version archivo:")
-    println var_version
-
-    print("Nueva Version archivo:")
-
-    println var_newversion
 
     File file_update = new File(archivo)
     file_update.write(var_newversion);
