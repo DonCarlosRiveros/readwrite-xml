1476441b3fc036f25ad8508d4fc9669cc06411c0
Carlos M. Riveros C <carlos.riveros@gmail.com>
2021-01-08 10:10:19 -0300

    Actualizacion con contenido local

---
+++ vars/ejecucion.groovy
@@ -0,0 +1,18 @@
+import groovy.xml.MarkupBuilder 
+import groovy.util.*
+
+def call()
+{
+	def parser = new XmlParser()
+	def doc    = parser.parse("resources/versiones.xml")
+
+	String var_version = ''
+		
+    var_version = ${bk['@version']
+    doc.movie.each
+    {
+    	bk->
+    	print("Version archivo:")
+    	println "${bk['@version']}"
+    }
+} 
